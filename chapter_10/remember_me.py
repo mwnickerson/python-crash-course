cba9dadf93afa1efcc0a49b64cee2728f6200142
mattn <mwnickerson@gmail.com>
2021-09-13 15:48:30 -0400

    Chapter 10: Storing Data Saving and Reading User-Generated Data version 3 Refactored by creating functions to call instead

--- chapter_10/remember_me.py
+++ chapter_10/remember_me.py
@@ -1,18 +1,37 @@
 # Remember Me
 # Chapter 10: Storing Data
-# Saving and Reading User-Generated Data
+# refactoring
 import json
 
-# Load the username if it has been stored previously.
-# Otherwise, prompt for the username and store it
-filename = 'jsons/username2.json'
-try:
-    with open(filename) as f:
-        username = json.load(f)
-except FileNotFoundError:
+def get_stored_username():
+    """Get stored username if available."""
+    filename = 'jsons/username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def get_new_username():
+    """prompt for a new username"""
     username = input("What is your name? ")
+    filename = 'jsons/username.json'
     with open(filename, 'w') as f:
         json.dump(username, f)
-        print(f"We'll remember you when you come back, {username}!")
-else:
-    print(f"Welcome back, {username}!")
+    return username
+
+def greet_user():
+    """Greet the user by name."""
+    username = get_stored_username()
+    if username:
+        print(f"Welcome back, {username}!")
+    else:
+        username = get_new_username()
+        filename = 'jsons/username.json'
+        with open(filename, 'w') as f:
+            json.dump(username, f)
+            print(f"We'll remember you when you come back, {username}!")
+
+greet_user()
