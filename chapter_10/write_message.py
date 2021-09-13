cab468b3563ecd0e1b62e754c66ca1d447370045
mattn <mwnickerson@gmail.com>
2021-09-13 10:53:43 -0400

    Chapter 10: Writing to Files writing multiple lines

--- chapter_10/write_message.py
+++ chapter_10/write_message.py
@@ -1,8 +1,17 @@
 # Write Message
 # Chapter 10
+# version 1
 # Writing to an empty file
+#filename = 'text_files/programming.txt'
+#
+#with open(filename, 'w') as file_object:
+#    file_object.write("I love programming!")
+
+# version 2
+# Writing Multiple lines
 filename = 'text_files/programming.txt'
 
 with open(filename, 'w') as file_object:
-    file_object.write("I love programming!")
+    file_object.write("I love programming!\n")
+    file_object.write("I love creating games!\n")
 
