dd81ed8da228b3528054916e0fd6321e64088265
mattn <mwnickerson@gmail.com>
2021-09-13 10:20:36 -0400

    version 2: working with large files

--- chapter_10/pi_string.py
+++ chapter_10/pi_string.py
@@ -3,14 +3,29 @@
 # version 1
 # Working with a file's contents
 
-filename = 'text_files/pi_digits.txt'
+#filename = 'text_files/pi_digits.txt'
+
+#with open(filename) as file_object:
+#    lines = file_object.readlines()
+
+#pi_string = ''
+#for line in lines:
+#    pi_string += line.rstrip()
+
+#print(pi_string)
+#print(len(pi_string))
+
+
+# version 2
+# Large files: one million digits
+filename = 'text_files/pi_million_digits.txt'
 
 with open(filename) as file_object:
     lines = file_object.readlines()
 
 pi_string = ''
 for line in lines:
-    pi_string += line.rstrip()
+    pi_string += line.strip()
 
-print(pi_string)
+print(f"{pi_string[:52]}...")
 print(len(pi_string))
