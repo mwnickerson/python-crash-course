b0523c7aa04671e5c18e3b751bb20a35fcf96b62
mattn <mwnickerson@gmail.com>
2021-09-13 10:56:23 -0400

    Chapter 10: Writing to Files appending to a file

--- chapter_10/write_message.py
+++ chapter_10/write_message.py
@@ -9,9 +9,17 @@
 
 # version 2
 # Writing Multiple lines
+#filename = 'text_files/programming.txt'
+#
+#with open(filename, 'w') as file_object:
+#    file_object.write("I love programming!\n")
+#    file_object.write("I love creating games!\n")
+
+# version 3
+# appending to a file
 filename = 'text_files/programming.txt'
 
-with open(filename, 'w') as file_object:
-    file_object.write("I love programming!\n")
-    file_object.write("I love creating games!\n")
+with open(filename, 'a') as file_object:
+    file_object.write("I also love finding meaning in a large dataset.\n")
+    file_object.write("I love creating apps that can run in a browser.\n")
 
