646a514fda0e8aa16947612069eb9f039a96b743
mattn <mwnickerson@gmail.com>
2021-09-13 09:50:44 -0400

    reading line by line

--- chapter_10/file_reader.py
+++ chapter_10/file_reader.py
@@ -2,7 +2,16 @@
 # Chapter 10
 # reads and prints the contents of a file
 
-with open('text_files/pi_digits.txt') as file_object:
-    contents = file_object.read()
-    print(contents.rstrip())
+# Version 1
+# with open('text_files/pi_digits.txt') as file_object:
+#    contents = file_object.read()
+#    print(contents.rstrip())
+
+# Version 2
+# reading line by line
+filename = 'text_files/pi_digits.txt'
+
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.rstrip())
 
