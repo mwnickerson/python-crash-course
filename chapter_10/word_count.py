0e619c01e79897e9da4b16b11272fa20070fc14b
mattn <mwnickerson@gmail.com>
2021-09-13 12:15:01 -0400

    Chapter 10: Exceptions Working with multiple files

---
+++ chapter_10/word_count.py
@@ -0,0 +1,21 @@
+# Word Count
+# Chapter 10: Exceptions
+# Working with multiple files
+
+def count_words(filename):
+    """Counts the approximate number of words in a file"""
+    try:
+        with open(filename, encoding='utf-8') as f:
+            contents = f.read()
+    except FileNotFoundError:
+        print(f"Sorry, the file {filename} was not found")
+    else:
+        words = contents.split()
+        num_words = len(words)
+        print(f"The file {filename} has {num_words} words.")
+
+filenames = ['text_files/alice.txt', 'text_files/siddartha.txt',\
+             'text_files/moby_dick.txt', 'text_files/little_women.txt']
+
+for filename in filenames:
+    count_words(filename)
