44bb3586ca709b99b8e4092e1c806be0ae9aea4d
mattn <mwnickerson@gmail.com>
2021-09-13 11:45:11 -0400

    Chapter 10: Exceptions using exceptions to prevent crashes the else block

--- chapter_10/division_calculator.py
+++ chapter_10/division_calculator.py
@@ -6,7 +6,26 @@
 # this throws a zerodivisionerror
 
 # using try except blocks
-try:
-    print(5/0)
-except ZeroDivisionError:
-    print("You can't divide by zero!")
+#try:
+#    print(5/0)
+#except ZeroDivisionError:
+#    print("You can't divide by zero!")
+
+# using exceptions to prevent crashes
+# the else block
+
+print("Give me two numbers, and I'll divide them.")
+print("Enter 'q' to quit")
+while True:
+    first_number = input("\nFirst number: ")
+    if first_number == 'q':
+        break
+    second_number = input("Second number: ")
+    if second_number == 'q':
+        break
+    try:
+        answer = int(first_number) / int(second_number)
+    except ZeroDivisionError:
+        print("You can't divide by zero!")
+    else:
+        print(answer)
