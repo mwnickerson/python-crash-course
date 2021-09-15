cfd86b7a36e90a7f0383fb0466fa2907cd3258b7
Matthew <82531659+mwnickerson@users.noreply.github.com>
2021-09-15 16:57:49 -0400

    Alien Invasion Game Piloting the Ship moving the ship both left and right

--- Alien_Invasion/ship.py
+++ Alien_Invasion/ship.py
@@ -20,11 +20,13 @@ class Ship:
 
         # movement flag
         self.moving_right = False
-
+        self.moving_left = False
     def update(self):
         """Update the ship's position based on the movement flag."""
         if self.moving_right:
             self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         """draw the ship at its current location."""
