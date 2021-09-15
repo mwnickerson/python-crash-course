7b21f7174a141136c68ae0a58782ae52238f66de
Matthew <82531659+mwnickerson@users.noreply.github.com>
2021-09-15 15:12:42 -0400

    Alien Invasion Game adds the player ship to the main game file

--- Alien_Invasion/alien_invasion.py
+++ Alien_Invasion/alien_invasion.py
@@ -3,7 +3,9 @@
 
 import sys
 import pygame
+
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
@@ -17,8 +19,11 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
         # Set the background color
         self.bg_color = (230, 230, 230)
+
 
     def run_game(self):
         """Start the main game loop"""
@@ -30,6 +35,7 @@ class AlienInvasion:
 
             # redraw the screen during each pass through the loop
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # make the most recently drawn screen visible.
             pygame.display.flip()
