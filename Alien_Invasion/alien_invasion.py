08dc296ae1e5e66b9b3c8b939ebb101f2eb117ea
Matthew <82531659+mwnickerson@users.noreply.github.com>
2021-09-15 16:48:04 -0400

    Alien Invasion Game Piloting the Ship Allowing continuous movement

--- Alien_Invasion/alien_invasion.py
+++ Alien_Invasion/alien_invasion.py
@@ -29,6 +29,7 @@ class AlienInvasion:
         """Start the main game loop"""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -39,7 +40,10 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     # Move the ship to the right
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
