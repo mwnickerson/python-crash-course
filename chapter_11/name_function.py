e3936fcc21717d995ea077d082f367b4402f8be6
Matthew <82531659+mwnickerson@users.noreply.github.com>
2021-09-14 20:54:25 -0400

    Chapter 11: Testing a Function A failing function repair the function when it fails

--- chapter_11/name_function.py
+++ chapter_11/name_function.py
@@ -3,8 +3,11 @@
 # Testing a function
 # function to import
 
-def get_formatted_name(first, last):
+def get_formatted_name(first, last, middle=''):
     """generate a neatly formatted full name."""
-    full_name = f"{first} {last}"
+    if middle:
+        full_name = f"{first} {middle} {last}"
+    else:
+        full_name = f"{first} {last}"
     return full_name.title()
 
