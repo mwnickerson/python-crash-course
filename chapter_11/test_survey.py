806c89a51139b7ad9ad016df7e9345ce810b5ab4
Matthew <82531659+mwnickerson@users.noreply.github.com>
2021-09-15 12:43:38 -0400

    Chapter 11: Testing a class adding a second test for the class

--- chapter_11/test_survey.py
+++ chapter_11/test_survey.py
@@ -14,5 +14,19 @@ class TestAnonymousSurvey(unittest.TestCase):
         my_survey = AnonymousSurvey(question)
         my_survey.store_response('English')
         self.assertIn('English', my_survey.responses)
+
+    # adding a second test for multiple inputs
+    def test_store_three_responses(self):
+        """Test that multiple responses are stored properly"""
+        question = "What language did you first learn to speak?"
+        my_survey = AnonymousSurvey(question)
+        responses = ['English', 'Spanish', 'Chinese']
+        for response in responses:
+            my_survey.store_response(response)
+
+        for response in responses:
+            self.assertIn(response, my_survey.responses)
+
+
 if __name__ == '__main__':
     unittest.main()
