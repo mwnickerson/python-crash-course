0ea0154f89a3911eccbddae966378a51ce64dae0
Matthew <82531659+mwnickerson@users.noreply.github.com>
2021-09-16 14:38:06 -0400

    Shooting Bullets: Creating the _update_bullets() method

--- chapter_12/Alien_Invasion/alien_invasion.py
+++ chapter_12/Alien_Invasion/alien_invasion.py
@@ -34,13 +34,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-
-            # get rid of bullets that have dissapeared
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-                print(len(self.bullets))
+            self._update_bullets()
 
             self._update_screen()
 
@@ -79,6 +73,14 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """update the position of bullets and get rid of old bullets"""
+        self.bullets.update()
+        # get rid of bullets that have dissapeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
