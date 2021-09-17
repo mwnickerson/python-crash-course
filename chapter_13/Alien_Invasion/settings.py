77ee9869012e2fa200dda657b0e9e90ef2cb3140
Matthew <82531659+mwnickerson@users.noreply.github.com>
2021-09-17 11:37:43 -0400

    chapter 13: making the fleet move - creating settings for fleet direction

--- chapter_13/Alien_Invasion/settings.py
+++ chapter_13/Alien_Invasion/settings.py
@@ -22,6 +22,11 @@ class Settings:
 
         # alien settings
         self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # 1 represents movement right -1 represents movement left
+        self.fleet_direction =1
+
+
 
 
 
