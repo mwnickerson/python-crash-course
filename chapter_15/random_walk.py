ee255d6e3f019b6f7cff7753f9a21ef6dee5f908
Matthew <82531659+mwnickerson@users.noreply.github.com>
2021-09-20 14:48:26 -0400

    chapter 15: Random Walks - Creating the randomwalk class

---
+++ chapter_15/random_walk.py
@@ -0,0 +1,15 @@
+# Random Walk
+# Chapter 15
+
+from random import choice
+
+class RandomWalk:
+    """a class to generate random walks"""
+
+    def __init__(self, num_points=5000):
+        """initialize the attributes of a walk"""
+        se;f.num_points
+
+        # all walks start at (0,0)
+        self.x_values = [0]
+        self.y_values = [0]
