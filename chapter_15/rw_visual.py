73f5949a6ed43ff2803f90c2827d3fb7fe467e38
Matthew <82531659+mwnickerson@users.noreply.github.com>
2021-09-20 16:15:33 -0400

    chapter 15: Random Walks - Styling the walk - plotting the starting and ending points

--- chapter_15/rw_visual.py
+++ chapter_15/rw_visual.py
@@ -5,12 +5,25 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# make a random walk
-rw = RandomWalk()
-rw.fill_walk()
-# plot the points of the random walk
+# keep making random walks as long as the program is active
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
+    # plot the points of the random walk
 
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers,
+               cmap=plt.cm.Blues, edgecolors='none', s=15)
+
+    # emphasize the first and last position
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red',
+               edgecolors='none', s=100)
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
+
