a264c73bab491435848cba7fc28b10ccb8d23ec2
Matthew <82531659+mwnickerson@users.noreply.github.com>
2021-09-20 17:43:19 -0400

    chapter 15: Exercises 15-3, 15-4, 15-5

--- chapter_15/random_walk.py
+++ chapter_15/random_walk.py
@@ -14,23 +14,24 @@ class RandomWalk:
         self.x_values = [0]
         self.y_values = [0]
 
+    def get_step(self):
+        """determine the direction and distance for a step"""
+        direction = choice([1, -1])
+        distance = choice([0, 1, 2, 3, 4, 5])
+        step = distance * direction
+        return step
+
     def fill_walk(self):
         """calculate all points in the walk"""
 
         # keep taking steps until the walk reaches the desired length
         while len(self.x_values) < self.num_points:
-
             # decide which direction to go and how far to go
-            x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
 
             # reject moves that go nowhere
-            if x_step == 0 and y_step == 0 :
+            if x_step == 0 and y_step == 0:
                 continue
             # calculate new position
             x = self.x_values[-1] + x_step
