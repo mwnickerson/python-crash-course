f40feef6b5df75c605029fdcc7c08d9108f321b6
Matthew <82531659+mwnickerson@users.noreply.github.com>
2021-09-20 11:37:21 -0400

    chapter 15: Plotting a simple line graph: plotting an series of points with scatter()

---
+++ chapter_15/scatter_squares.py
@@ -0,0 +1,22 @@
+# scatter squares
+# chapter 15
+# plotting and styling individual points with scatter()
+
+import matplotlib.pyplot as plt
+
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, s=100)
+
+# set chart title and label
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# set size of tick labels
+ax.tick_params(axis='both', which='major', labelsize=14)
+
+plt.show()
