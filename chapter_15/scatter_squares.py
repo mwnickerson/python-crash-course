952b3d100cb6ef59f94fbb36a0ac0b9f66a809da
Matthew <82531659+mwnickerson@users.noreply.github.com>
2021-09-20 11:40:10 -0400

    chapter 15: Plotting a simple line graph: calculating data automatically

--- chapter_15/scatter_squares.py
+++ chapter_15/scatter_squares.py
@@ -4,17 +4,20 @@
 
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, s=10)
 
 # set chart title and label
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
+
+# set the range for each axis
+ax.axis([0, 1100, 0, 1_100_000])
 
 # set size of tick labels
 ax.tick_params(axis='both', which='major', labelsize=14)
