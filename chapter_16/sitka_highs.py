40c7ad3f9c4f1e4c4a8bd0f0a92b30a7399cc587
Matthew <82531659+mwnickerson@users.noreply.github.com>
2021-09-21 13:19:57 -0400

    Chapter 16: The CSV file format - plotting dates

--- chapter_16/sitka_highs.py
+++ chapter_16/sitka_highs.py
@@ -1,6 +1,7 @@
 # sitka highs
 
 import csv
+from datetime import datetime
 
 import matplotlib.pyplot as plt
 
@@ -9,20 +10,23 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row =next(reader)
 
-    # get high temps from the file
-    highs = []
+    # get dates and high temps from the file
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high =int(row[5])
+        dates.append(current_date)
         highs.append(high)
 
 # plot the temperature
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(highs, c='red')
+ax.plot(dates, highs, c='red')
 
 # format plot
 ax.set_title("Daily high temperatures, July 2018", fontsize=24)
 ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
 ax.tick_params(axis='both', which='major', labelsize=16)
 
