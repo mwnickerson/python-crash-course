f1be6941fb81f1d2169242a034e9c7e66d25042e
Matthew <82531659+mwnickerson@users.noreply.github.com>
2021-09-21 13:42:59 -0400

    Chapter 16: The CSV file format - plotting a second data series

--- chapter_16/sitka_2018_highs.py
+++ chapter_16/sitka_2018_highs.py
@@ -10,21 +10,24 @@ with open(filename) as f:
     reader =csv.reader(f)
     header_row = next(reader)
 
-    # get dates and high temps from the file
-    dates, highs = [], []
+    # get dates and high and low temps from the file
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
+        low = int(row[6])
         dates.append(current_date)
         highs.append(high)
+        lows.append(low)
 
-# plot the temps
+# plot the high and low temps
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red')
+ax.plot(dates, lows, c='blue')
 
 # format plot
-ax.set_title("Daily High temperatures, - 2018", fontsize=24)
+ax.set_title("Daily High and low temperatures, - 2018", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
