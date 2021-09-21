1b20df1716a4f1e0a8991b4e520e01551ed2a9ab
Matthew <82531659+mwnickerson@users.noreply.github.com>
2021-09-21 13:10:31 -0400

    Chapter 16: The CSV file format - Plotting data in a temperature chart

--- chapter_16/sitka_highs.py
+++ chapter_16/sitka_highs.py
@@ -2,6 +2,8 @@
 
 import csv
 
+import matplotlib.pyplot as plt
+
 filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
@@ -13,4 +15,15 @@ with open(filename) as f:
         high =int(row[5])
         highs.append(high)
 
-print(highs)
+# plot the temperature
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+# format plot
+ax.set_title("Daily high temperatures, July 2018", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
