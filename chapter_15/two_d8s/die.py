e0986f57e009746e04bce18090369a7aa7d2150e
Matthew <82531659+mwnickerson@users.noreply.github.com>
2021-09-21 10:27:22 -0400

    chapter 15: Exercise 6 - Two D8s

---
+++ chapter_15/two_d8s/die.py
@@ -0,0 +1,13 @@
+# die class
+from random import randint
+
+class Die:
+    """a class to simulate a die"""
+
+    def __init__(self, num_sides=6):
+        """assume a six sided die"""
+        self.num_sides = num_sides
+
+    def roll(self):
+        """return a random value b/n 1 and # of sides"""
+        return randint(1, self.num_sides)
