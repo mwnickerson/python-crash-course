d8d51e0a45d461374aec581425cf45a33d9552df
Matthew <82531659+mwnickerson@users.noreply.github.com>
2021-09-22 15:07:45 -0400

    Chapter 17: Using a Web API - summarizing the top repositories

--- chapter_17/python_repos.py
+++ chapter_17/python_repos.py
@@ -14,7 +14,7 @@ print(f"Total repositories: {response_dict['total_count']}")
 
 # explore information about the repositories
 repo_dicts = response_dict['items']
-print(f"Repositories return: {len(repo_dicts)}")
+print(f"Repositories returned: {len(repo_dicts)}")
 
 # examine the first repo
 repo_dict = repo_dicts[0]
@@ -23,13 +23,14 @@ repo_dict = repo_dicts[0]
 #    print(key)
 
 print("\nSelected information about first repository:")
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stars: {repo_dict['stargazers_count']}")
-print(f"Repository: {repo_dict['html_url']}")
-print(f"Created: {repo_dict['created_at']}")
-print(f"Updated: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
+for repo_dict in repo_dicts:
+    print(f"\nName: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
 
 
 
