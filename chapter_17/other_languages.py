696d4bf0124052288cb65e3c60cbc1f5aefeada3
Matthew <82531659+mwnickerson@users.noreply.github.com>
2021-09-22 17:48:08 -0400

    Chapter 17 exercise 1: other languages

---
+++ chapter_17/other_languages.py
@@ -0,0 +1,38 @@
+# Chapter 17 exercise 1
+# using github api to identfiy top repos in other languages
+
+import requests
+
+languages = ['go', 'javascript', 'ruby', 'rust', 'java', 'haskell', 'c']
+for language in languages:
+    print("-------------------------------------------------------------------------------------")
+    print(f"\nTHE TOP REPOSITORIES FOR {language}")
+    # make an API call and store the response
+    url = f'https://api.github.com/search/repositories?q=language:{language}&sort=stars'
+    headers = {'Accept': 'application/vnd.github.v3+json'}
+    r = requests.get(url, headers=headers)
+    print(f"Status Code: {r.status_code}")
+
+    # store the API repsonses in a variable
+    response_dict =r.json()
+    print(f"Total repositories: {response_dict['total_count']}")
+
+    # explore information about the repositories
+    repo_dicts = response_dict['items']
+    print(f"Repositories returned: {len(repo_dicts)}")
+
+    # examine the first repo
+    repo_dict = repo_dicts[0]
+    # print(f"\nKeys: {len(repo_dict)}")
+    # for key in sorted(repo_dict.keys()):
+    #    print(key)
+
+    print("\nSelected information about first repository:")
+    for repo_dict in repo_dicts:
+        print(f"\nName: {repo_dict['name']}")
+        print(f"Owner: {repo_dict['owner']['login']}")
+        print(f"Stars: {repo_dict['stargazers_count']}")
+        print(f"Repository: {repo_dict['html_url']}")
+        print(f"Created: {repo_dict['created_at']}")
+        print(f"Updated: {repo_dict['updated_at']}")
+        print(f"Description: {repo_dict['description']}")
