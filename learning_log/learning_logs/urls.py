50bda360ac61c7bd861cb97732e8987edb142f4b
Matthew <82531659+mwnickerson@users.noreply.github.com>
2021-09-23 15:32:50 -0400

    chapter_18: Making Pages: The learning log home page - mapping a url

---
+++ learning_log/learning_logs/urls.py
@@ -0,0 +1,10 @@
+"""defines url patterns for learning_logs"""
+
+from django.urls import path
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+
+    path('', views.index, name='index'),
+]
