5ad64e11e1ee2441ced096643975ebf62835218a
Matthew <82531659+mwnickerson@users.noreply.github.com>
2021-09-23 16:23:21 -0400

    chapter_18: Making Pages: Building Additional Pages- The Topics Page - The topics url pattern

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 app_name = 'learning_logs'
 urlpatterns = [
-
+    # home page
     path('', views.index, name='index'),
+    # topics page
+    path('topics/', views.topics, name='topics'),
 ]
