d9a4a4d48a745373c856a2e2e8339f6b3c15783d
Matthew <82531659+mwnickerson@users.noreply.github.com>
2021-09-24 16:07:26 -0400

    Chapter 18: Meal planning exercise - create a home page for a meal planning app

---
+++ chapter_18_exercises/pizzeria_project/pizzas/urls.py
@@ -0,0 +1,11 @@
+"""defines url patterns for pizzas"""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'pizzas'
+urlpatterns = [
+    # home page
+    path('', views.index, name='index')
+]
