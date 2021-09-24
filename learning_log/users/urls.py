f38a5762a724e736586fae780fd1302c9a942a76
Matthew <82531659+mwnickerson@users.noreply.github.com>
2021-09-24 13:05:27 -0400

    Chapter 19: Setting up User Accounts: The users app - The login page

---
+++ learning_log/users/urls.py
@@ -0,0 +1,9 @@
+"""defines the url patterns for users"""
+
+from django.urls import path, include
+
+app_name = 'users'
+urlpatterns = [
+    # include default auth urls
+    path('', include('django.contrib.auth.urls')),
+]
